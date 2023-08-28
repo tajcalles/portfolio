77127a6d683d907f3d10f1b908a4b0887fe84ffd
Taj Calles <tajcalles@gmail.com>
2023-08-28 13:15:39 -0600

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,16 @@
+<<<<<<< HEAD
 <script setup>
 import { ref } from 'vue'
 
 defineProps({
   msg: String,
 })
+=======
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+>>>>>>> d73fa4b (init)
 
 const count = ref(0)
 </script>
